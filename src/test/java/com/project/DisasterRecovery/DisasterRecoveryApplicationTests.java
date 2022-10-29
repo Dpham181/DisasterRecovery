c6e3af18efd8900a8a56b27ef0346976f2ec354b
danhpham <danhpham312@gmail.com>
2022-10-29 13:44:16 -0700

    dev-init

---
+++ src/test/java/com/project/DisasterRecovery/DisasterRecoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.DisasterRecovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisasterRecoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
