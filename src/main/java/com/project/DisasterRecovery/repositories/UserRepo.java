5e5716d87d9e8bdbd02f1b80990ea3b146a80d4e
danhpham <danhpham312@gmail.com>
2022-10-31 12:35:54 -0700

    jwt commit

--- src/main/java/com/project/DisasterRecovery/repositories/UserRepo.java
+++ src/main/java/com/project/DisasterRecovery/repositories/UserRepo.java
@@ -1,9 +1,12 @@
 package com.project.DisasterRecovery.repositories;
 
 import com.project.DisasterRecovery.Entities.EndUser;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepo extends JpaRepository<EndUser, Integer>{
 
-
+	 @Query(value = "Select * from end_user U Where U.email =?1", nativeQuery = true)
+	 EndUser loadUserByUsername(String email);
 }
