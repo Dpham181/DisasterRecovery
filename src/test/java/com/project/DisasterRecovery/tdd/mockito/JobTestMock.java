be554e1230dfa36819c67bab14654f7d8086ba78
Diep La <diepla@my.smccd.edu>
2022-10-31 13:07:31 -0700

    Create JobTestMock.java

---
+++ src/test/java/com/project/DisasterRecovery/tdd/mockito/JobTestMock.java
@@ -0,0 +1,27 @@
+package com.project.DisasterRecovery.tdd.mockito;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.project.DisasterRecovery.Services.JobServices;
+import com.project.DisasterRecovery.repositories.JobRepo;
+
+public class JobTestMock {
+
+	@Mock 
+	JobRepo jobRepo;
+	
+	@InjectMocks
+	@Autowired
+	JobServices jobServices;
+	
+	@Test
+	public <Job> getJobList()
+	{
+		
+	}
+}
