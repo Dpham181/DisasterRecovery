96cfa8b21bde9d44abba1b91321db4a6711a1eba
Diep La <diepla@my.smccd.edu>
2022-10-31 23:43:43 -0700

    update add services

--- src/main/java/com/project/DisasterRecovery/repositories/JobRepo.java
+++ src/main/java/com/project/DisasterRecovery/repositories/JobRepo.java
@@ -1,6 +1,7 @@
 package com.project.DisasterRecovery.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.project.DisasterRecovery.Entities.Job;
@@ -8,4 +9,6 @@ import com.project.DisasterRecovery.Entities.Job;
 @Repository
 public interface JobRepo extends JpaRepository<Job, Integer>{
 
+	 @Query(value = "Select * from job j Where j.code =?1", nativeQuery = true)
+	 Job loadJobByCode(String code);
 }
--- src/main/java/com/project/DisasterRecovery/repositories/TimeCardRepo.java
+++ src/main/java/com/project/DisasterRecovery/repositories/TimeCardRepo.java
@@ -1,11 +1,15 @@
 package com.project.DisasterRecovery.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.project.DisasterRecovery.Entities.Job;
 import com.project.DisasterRecovery.Entities.TimeCard;
 
 @Repository
 public interface TimeCardRepo extends JpaRepository<TimeCard, Integer>{
 
+	 @Query(value = "Select * from timecard tc Where tc.code =?1", nativeQuery = true)
+	 TimeCard loadTimeCardByCode(String code);
 }
