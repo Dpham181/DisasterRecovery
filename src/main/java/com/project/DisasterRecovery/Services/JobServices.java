d3d389f91882499aa8bef041f0c3756b52033ecd
danhpham <danhpham312@gmail.com>
2022-11-01 12:10:13 -0700

    init  exception
    changes:
    1. modified repo
    2. modified serveries
    3. modified controller
    Job and User completed
    task machine and timecard

--- src/main/java/com/project/DisasterRecovery/Services/JobServices.java
+++ src/main/java/com/project/DisasterRecovery/Services/JobServices.java
@@ -2,6 +2,9 @@ package com.project.DisasterRecovery.Services;
 
 import java.util.List;
 
+import com.project.DisasterRecovery.Entities.EndUser;
+import com.project.DisasterRecovery.exception.DuplicateException;
+import com.project.DisasterRecovery.exception.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -23,22 +26,27 @@ public class JobServices {
     }
     
     // get one job
-    public ResponseEntity<Job> getOneJob(int id) {
-        if(jobRepo.existsById(id)){
-            return ResponseEntity.ok().body(jobRepo.findById(id).get());
-        }
-        return ResponseEntity.notFound().build();
+    public ResponseEntity<Job> getOneJob(int id)  throws NotFoundException {
+        Job job = jobRepo.findById(id).orElseThrow(()-> new NotFoundException("Job not found "+ id));
+       return ResponseEntity.ok(job);
     }
     
     // create job
-    public ResponseEntity<Job> createJob(Job job){
-      
-        jobRepo.save(job);
-        return ResponseEntity.status(201).build();
+    public ResponseEntity<Job> createJob(Job job) throws DuplicateException {
+
+        Job JOB = jobRepo.loadJobByJobCode(job.getCode());
+        int id = 0;
+        if(JOB == null ) {
+            id =  jobRepo.save(job).getId();
+
+        }
+        Job ExitsJob =  jobRepo.findById(id).orElseThrow(() -> new DuplicateException("Job already exists :: " + job.getCode() ));
+
+        return ResponseEntity.ok(ExitsJob);
     }
     
     // update job
-    public ResponseEntity<Job> updateJob(int id, Job job){
+    public ResponseEntity<Job> updateJob(int id, Job job) throws NotFoundException {
         if(getOneJob(id).hasBody())
         {
         	Job modifiedJob = getOneJob(id).getBody();
@@ -52,8 +60,7 @@ public class JobServices {
     }
     
     // delete job
-    public ResponseEntity<Job> deleteJob(int id)
-    {
+    public ResponseEntity<Job> deleteJob(int id) throws NotFoundException {
     	if(getOneJob(id).hasBody())
     	{
     		jobRepo.deleteById(id);
--- src/main/java/com/project/DisasterRecovery/controller/JobController.java
+++ src/main/java/com/project/DisasterRecovery/controller/JobController.java
@@ -3,6 +3,8 @@ package com.project.DisasterRecovery.controller;
 
 import com.project.DisasterRecovery.Entities.Job;
 import com.project.DisasterRecovery.Services.JobServices;
+import com.project.DisasterRecovery.exception.DuplicateException;
+import com.project.DisasterRecovery.exception.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,19 +27,18 @@ public class JobController {
       }
 
       @PostMapping("")
-      public ResponseEntity<?> createJob(@RequestBody Job job){
+      public ResponseEntity<?> createJob(@RequestBody Job job) throws DuplicateException {
             return JobServices.createJob(job);
       }
 
       @PutMapping ("/{id}")
-      public ResponseEntity<?> UpdateJob(@PathVariable Integer id, @RequestBody Job job){
+      public ResponseEntity<?> UpdateJob(@PathVariable Integer id, @RequestBody Job job) throws NotFoundException {
             return JobServices.updateJob(id,job);
       }
 
 
       @GetMapping ("/{id}")
-      public ResponseEntity<?> getOneJob(@PathVariable int id){
-            System.out.println(id);
+      public ResponseEntity<?> getOneJob(@PathVariable int id) throws NotFoundException {
             return JobServices.getOneJob(id);
       }
 
